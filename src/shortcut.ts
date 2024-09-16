ae585aa62a6d4ccb4cb7bc9c7923679d0c102ca1
yutankaifa <1989541473@qq.com>
2024-09-16 20:47:00 +0800

    fix: show selection app once

--- src/shortcut.ts
+++ src/shortcut.ts
@@ -5,43 +5,46 @@ import {
   unregisterAll,
 } from "@tauri-apps/plugin-global-shortcut";
 import { shortcuts } from "./common/data.ts";
-import {
-  getAllWebviewWindows,
-  WebviewWindow,
-} from "@tauri-apps/api/webviewWindow";
+import { WebviewWindow } from "@tauri-apps/api/webviewWindow";
+import { invoke } from "@tauri-apps/api/core";
 
 export const registerShortcut = async (shortcut: string) => {
   try {
+    const flag = await invoke("is_created_selection");
+    let selectionWindow: WebviewWindow;
+    if (!flag) {
+      selectionWindow = new WebviewWindow("selection", {
+        url: "../selection.html",
+        decorations: false,
+        fullscreen: true,
+        resizable: false,
+        transparent: true,
+        alwaysOnTop: true,
+        visible: false,
+        shadow: false,
+        theme: "dark",
+        skipTaskbar: true,
+      });
+      await selectionWindow.once("tauri://created", () => {
+        console.log("The selection window has been created");
+      });
+      await selectionWindow.once("tauri://error", (e) => {
+        console.error("Error creating selection window:", e);
+      });
+    }
     const res = await isRegistered(shortcut);
     console.log("shortcut", res, shortcut);
     if (!res) {
       await register(shortcut, async () => {
         if (shortcut == shortcuts[0].name) {
-          let selectionWindow = new WebviewWindow("selection", {
-            // url: "src/selection/selection.html",
-            url: "../selection.html",
-            decorations: false,
-            fullscreen: true,
-            resizable: false,
-            transparent: true,
-            alwaysOnTop: true,
-            visible: true,
-            shadow: false,
-            theme: "dark",
-            skipTaskbar: true,
-          });
-          await selectionWindow.once("tauri://created", () => {
-            console.log("The selection window has been created");
-          });
-          await selectionWindow.once("tauri://error", (e) => {
-            console.error("Error creating selection window:", e);
-          });
+          await selectionWindow.show();
         } else if (shortcut == shortcuts[1].name) {
-          const webViews = await getAllWebviewWindows();
-          const selectionWindow = webViews.find(
-            (item) => item.label == "selection",
-          );
-          if (selectionWindow) await selectionWindow.close();
+          // const webViews = await getAllWebviewWindows();
+          // const selectionWindow = webViews.find(
+          //   (item) => item.label == "selection",
+          // );
+          // if (selectionWindow) await selectionWindow.close();
+          await selectionWindow.close();
         }
       });
     }
